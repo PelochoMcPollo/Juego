7123b728a81c29efeb9f6b959ef5c28700f2b123
PelochoMcPollo <eduard01kate@gmail.com>
2026-09-16 15:04:33 +0200

    feat: añadir salto al jugador
    Añadimos la mecánica de saltar pulsando el espacio y arreglamos un bug de la velocidad del jugador, al ir en diagonal iba mas rápido  (√(1² + 1²) = √2 ≈ 1.41)
    Fixeado poniendo que lo maximo que puede ir es 1

--- Assets/_Proyecto/Jugador/MovimientoJugador.cs
+++ Assets/_Proyecto/Jugador/MovimientoJugador.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class MovimientoJugador: MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Movimiento 
     [SerializeField] private float velocidad = 5f;
-    [SerializeField] private Transform camara;
-    private CharacterController characterController;
     private Vector3 direccion = Vector3.zero;
+
+    //Salto
+    [SerializeField] private float fuerzaSalto = 5f;
+    [SerializeField] private float gravedad = 9.81f;
+    private float velocidadVertical;
+
+    //Camara 
+    [SerializeField] private Transform camara;
+
+    //Controlador 
+    private CharacterController characterController;
+
 
     void Awake()
     {
@@ -21,11 +31,25 @@ public class MovimientoJugador: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direccion = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        direccion = transform.TransformDirection(direccion);
-        direccion *= velocidad;
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        input = Vector3.ClampMagnitude(input, 1f);
+        Vector3 movimiento = transform.TransformDirection(input) * velocidad;
 
-        characterController.Move(direccion * Time.deltaTime);
+        if(characterController.isGrounded)
+        {
+            velocidadVertical = -2f;
 
+            if(Input.GetButtonDown("Jump"))
+            {
+                velocidadVertical = fuerzaSalto;
+            }
+        }
+        else
+        {
+            velocidadVertical -= gravedad * Time.deltaTime;
+        }
+
+        movimiento.y = velocidadVertical;
+        characterController.Move(movimiento * Time.deltaTime);
     }
 }
