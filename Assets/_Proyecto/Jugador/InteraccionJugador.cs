adedeb5dd11f56c9dde17a13a90defaba470ee57
PelochoMcPollo <eduard01kate@gmail.com>
2026-09-17 17:08:22 +0200

    feat: Añadir mecánica interactuar
    Añadir la mecánica de interactuar con objetos, si in objeto es interactivo, por la mascara y por la interfaz intratable, si el jugador esta a cierta distancia y pulsa la tecla E, interactúa con el objeto, ya sea una puerta, enchufe etc

---
+++ Assets/_Proyecto/Jugador/InteraccionJugador.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class InteraccionJugador : MonoBehaviour
+{
+    [SerializeField] private Transform camara;
+    [SerializeField] private float distanciaInteraccion = 5.0f;
+    [SerializeField] private LayerMask capaInteractuable;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(camara.position, camara.forward, out RaycastHit hit, distanciaInteraccion, capaInteractuable))
+            {
+                IInteractuable interactuable = hit.collider.GetComponent<IInteractuable>();
+                if (interactuable != null)
+                {
+                    interactuable.Interactuar();
+                }
+            }
+        }
+    }
+
+}
